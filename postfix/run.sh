b4491053db02e004da7707acfe7fc4d853d150e9
OMAR RAMON MONTES <omarmontes.879@gmail.com>
2023-12-10 10:48:57 -0500

    all sh

--- postfix/run.sh
+++ postfix/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo "Starting my mailserver..."
 
+debconf-set-selections <<< "postfix postfix/mailname string postfixasor.com"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+
+apt-get install --assume-yes postfix
+useradd -m -G mail -p pass1 user1 && touch /var/mail/user1 && chown user1:mail /var/mail/user1
+useradd -m -G mail -p pass2 user2 && touch /var/mail/user2 && chown user2:mail /var/mail/user2
+
+apt-get install rsyslog -y
+
 tail -f /var/log/mail.log

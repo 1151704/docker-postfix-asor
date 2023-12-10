2639e184e9b817c6705a7d1372d86c1af30c5ae3
OMAR RAMON MONTES <omarmontes.879@gmail.com>
2023-12-10 15:20:12 -0500

    restar

--- postfix/run.sh
+++ postfix/run.sh
@@ -3,9 +3,9 @@ cp /main.cf /etc/postfix/main.cf
 echo "disable_plaintext_auth = no" >> /etc/dovecot/conf.d/10-auth.conf
 echo "mail_location = maildir:~/Maildir" >> /etc/dovecot/conf.d/10-mail.conf
 
-service rsyslog start;
-service postfix start;
-service dovecot start;
+service rsyslog restart;
+service postfix restart;
+service dovecot restart;
 /bin/echo "FINALIZÓ TODO.";
 sleep 20;
 tail -f /var/log/mail.log

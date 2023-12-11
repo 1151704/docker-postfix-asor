1f8c0fc519798896f5cd31d3a9834fbe23a88890
OMAR RAMON MONTES <omarmontes.879@gmail.com>
2023-12-11 08:04:20 -0500

    config

--- roundcube/config-other.inc.php
+++ roundcube/config-other.inc.php
@@ -1,7 +1,6 @@
 <?php
   $config['default_host'] = 'postfixasor.local';
-  $config['smtp_server'] = 'postfixasor.local';
+  $config['smtp_server'] = 'smtp.postfixasor.local';
   $config['smtp_port'] = 25;
   $config['smtp_user'] = '';
   $config['smtp_pass'] = '%p';
-  $config['plugins'] = [];
